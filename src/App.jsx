32dfa0288d2c9c84087cd8350e5937fa5a1b6be2
abutalha08 <taufique752@gmail.com>
2026-04-26 21:58:36 +0600

    Add product form with validation and table display

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 // import ControlledField from './components/ControlledField/ControlledField'
 // import FamilyTree from './components/FamilyTree/FamilyTree'
 // import FormAction from './components/FormAction/FormAction'
-import HookForm from './components/HookForm/HookForm'
-// import ProductManagement from './components/ProductManagement/ProductManagement'
+// import HookForm from './components/HookForm/HookForm'
+import ProductManagement from './components/ProductManagement/ProductManagement'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 // import UnControlledField from './components/UnControlledField/UnControlledField'
 
@@ -16,8 +16,8 @@ function App() {
       {/* <FormAction></FormAction> */}
       {/* <ControlledField></ControlledField> */}
       {/* <UnControlledField></UnControlledField> */}
-      <HookForm></HookForm>
-      {/* <ProductManagement></ProductManagement> */}
+      {/* <HookForm></HookForm> */}
+      <ProductManagement></ProductManagement>
       {/* <FamilyTree></FamilyTree> */}
     </>
   )
